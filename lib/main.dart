ec6b2a91d4453a00062da07e8a537b6bef216a4d
carlstep <carlstep333@me.com>
2022-07-25 09:59:21 +0700

    added cached_network_image package

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,23 @@ void main() {
       appBar: AppBar(
         title: Text('Images in Flutter'),
       ),
+      body: ListView(
+        children: [
+          Image.network(
+            'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg',
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          Image.asset('assets/images/profile.jpg'),
+          Image(
+            image: AssetImage('assets/images/profile.jpg'),
+          ),
+        ],
+      ),
     ),
   ));
 }
