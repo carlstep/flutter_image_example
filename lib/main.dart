b8599d1798e9f25a5be5e626c1da071f7dbe6458
carlstep <carlstep333@me.com>
2022-07-26 09:38:49 +0700

    final code for image_example TUT

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,43 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text('Images in Flutter'),
+        title: const Text('Images in Flutter'),
       ),
       body: ListView(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: AssetImage('assets/images/profile.jpg'),
+                )
+              ],
+            ),
+          ),
+          Card(
+            clipBehavior: Clip.antiAlias,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: EdgeInsets.all(14),
+            elevation: 10,
+            child: Image.network(
+                'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg'),
+          ),
+          SizedBox(
+            height: 30,
+          ),
           CachedNetworkImage(
+            errorWidget: (context, url, error) => const Icon(
+              Icons.error_outlined,
+              size: 100,
+              color: Colors.red,
+            ),
             placeholder: (context, url) {
-              return Image(
+              return const Image(
                 image: AssetImage('assets/images/placeholder.png'),
               );
             },
@@ -22,13 +52,13 @@ void main() {
             'https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg',
             loadingBuilder: (context, child, loadingProgress) {
               if (loadingProgress == null) return child;
-              return Center(
+              return const Center(
                 child: CircularProgressIndicator(),
               );
             },
           ),
           Image.asset('assets/images/profile.jpg'),
-          Image(
+          const Image(
             image: AssetImage('assets/images/profile.jpg'),
           ),
         ],
